abe3b35f8f219d0998bcf01d36ee5513aa69578a
Jaromir Malenko <jaro@jaro.cz>
2025-02-20 17:43:22 +0100

    Add service for increasing counter

---
+++ src/test/java/cz/jaro/homework/TransactionServiceComponentTest.java
@@ -0,0 +1,45 @@
+package cz.jaro.homework;
+
+import cz.jaro.homework.model.Transaction;
+import cz.jaro.homework.repository.TransactionRepository;
+import cz.jaro.homework.service.TransactionService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TransactionServiceComponentTest {
+
+    @Mock
+    TransactionRepository transactionRepository;
+
+    @InjectMocks
+    TransactionService transactionService;
+
+    @Test
+    public void increaseCounters() {
+        long id = new Random().nextInt(10000);
+        Transaction transaction = new Transaction(id, System.currentTimeMillis(), "counter", "actor1", new ArrayList<>());
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+
+        when(transactionRepository.findAllByType(any(String.class))).thenReturn(transactions);
+
+        int count = transactionService.increaseCounters();
+
+        verify(transactionRepository, times(count)).findAllByType(any(String.class));
+
+        assertThat(count).isEqualTo(1);
+    }
+
+}
---
+++ src/test/java/cz/jaro/homework/TransactionServiceUnitTest.java
@@ -0,0 +1,62 @@
+package cz.jaro.homework;
+
+import cz.jaro.homework.model.KeyValue;
+import cz.jaro.homework.model.Transaction;
+import cz.jaro.homework.service.TransactionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class TransactionServiceUnitTest {
+
+    @Autowired
+    TransactionService transactionService;
+
+    @Test
+    void increaseCounter() throws Exception {
+        long id = new Random().nextInt(10000);
+        Transaction transaction = new Transaction(id, 101L, "counter", "actor1", new ArrayList<>());
+
+        transactionService.increaseCounter(transaction);
+
+        assertThat(transaction.getData().size()).isEqualTo(2);
+
+        KeyValue keyValueCounter = transactionService.getTransactionDataKey(transaction, "counter");
+        assertThat(keyValueCounter.getValue()).isEqualTo("1");
+
+        KeyValue keyValueModified = transactionService.getTransactionDataKey(transaction, "modified");
+        assertThat(keyValueModified).isNotNull();
+
+        transactionService.increaseCounter(transaction);
+
+        KeyValue keyValueCounter2 = transactionService.getTransactionDataKey(transaction, "counter");
+        assertThat(keyValueCounter2.getValue()).isEqualTo("2");
+
+        KeyValue keyValueModified2 = transactionService.getTransactionDataKey(transaction, "modified");
+        assertThat(keyValueModified2).isNotNull();
+    }
+
+    @Test
+    void increaseCounterNotANumber() throws Exception {
+        long id = new Random().nextInt(10000);
+        Transaction transaction = new Transaction(id, 101L, "counter", "actor1", new ArrayList<>());
+        transaction.addData(new KeyValue(null, null, "counter", "X"));
+
+        transactionService.increaseCounter(transaction);
+
+        assertThat(transaction.getData().size()).isEqualTo(2);
+
+        KeyValue keyValueCounter = transactionService.getTransactionDataKey(transaction, "counter");
+        assertThat(keyValueCounter.getValue()).isEqualTo("1");
+
+        KeyValue keyValueModified = transactionService.getTransactionDataKey(transaction, "modified");
+        assertThat(keyValueModified).isNotNull();
+    }
+
+}
