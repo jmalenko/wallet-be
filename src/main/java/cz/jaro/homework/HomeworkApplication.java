b3362fe1c672f6e3166aac7dd411eb9537d4f6e4
Jaromir Malenko <jaro@jaro.cz>
2025-02-20 17:44:17 +0100

    Schedule run of increasing counter service

--- src/main/java/cz/jaro/homework/HomeworkApplication.java
+++ src/main/java/cz/jaro/homework/HomeworkApplication.java
@@ -2,8 +2,10 @@ package cz.jaro.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HomeworkApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/cz/jaro/homework/schedule/IncreaseCountersSchedule.java
@@ -0,0 +1,24 @@
+package cz.jaro.homework.schedule;
+
+import cz.jaro.homework.service.TransactionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class IncreaseCountersSchedule {
+    private static final Logger log = LoggerFactory.getLogger(IncreaseCountersSchedule.class);
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Scheduled(fixedRate = 10000)
+    public void increaseCounters() {
+        log.info("Processing of new applications starts at " + new Date());
+        transactionService.increaseCounters();
+    }
+}
